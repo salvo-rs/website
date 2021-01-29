a4c92f70cabad0e887de73899683ab829153c9d1
chrislearn <chris@acroidea.com>
2021-01-29 14:47:01 +0800

    WIP

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,4 +1,24 @@
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const whitelister = require('purgecss-whitelister');
+
 module.exports = {
   plugins: [
+    autoprefixer(),
+    purgecss({
+      content: [
+        './layouts/**/*.html',
+        './content/**/*.md',   
+        './content/**/*.html',       
+      ],
+      safelist: [
+        'lazyloaded',
+        ...whitelister([
+          './assets/scss/components/_search.scss',
+          './assets/scss/components/_syntax.scss',
+          './assets/scss/common/_dark.scss',
+        ]),
+      ],
+    }),
   ],
 }
