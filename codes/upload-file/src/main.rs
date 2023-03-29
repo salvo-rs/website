14ce4656ad5fc0d5164ad49da731c96a5c3bd948
Chrislearn Young <chris@acroidea.com>
2023-03-29 16:57:29 +0800

    doc update

--- codes/upload-file/src/main.rs
+++ codes/upload-file/src/main.rs
@@ -12,7 +12,6 @@ async fn upload(req: &mut Request, res: &mut Response) {
     let file = req.file("file").await;
     if let Some(file) = file {
         let dest = format!("temp/{}", file.name().unwrap_or("file"));
-        println!("{}", dest);
         let info = if let Err(e) = std::fs::copy(&file.path(), Path::new(&dest)) {
             res.set_status_code(StatusCode::INTERNAL_SERVER_ERROR);
             format!("file not found in request: {}", e)
