54bf80fa4f677341f7b62c3f5de4d00fe625e977
Chrislearn Young <chris@acroidea.com>
2023-06-25 22:43:22 +0800

    update code

--- codes/jwt-auth/src/main.rs
+++ codes/jwt-auth/src/main.rs
@@ -1,6 +1,6 @@
 use jsonwebtoken::{self, EncodingKey};
 use salvo::http::{Method, StatusError};
-use salvo::jwt_auth::QueryFinder;
+use salvo::jwt_auth::{ConstDecoder, QueryFinder};
 use salvo::prelude::*;
 use serde::{Deserialize, Serialize};
 use time::{Duration, OffsetDateTime};
@@ -17,13 +17,14 @@ pub struct JwtClaims {
 async fn main() {
     tracing_subscriber::fmt().init();
 
-    let auth_handler: JwtAuth<JwtClaims> = JwtAuth::new(SECRET_KEY.to_owned())
-        .finders(vec![
-            // Box::new(HeaderFinder::new()),
-            Box::new(QueryFinder::new("jwt_token")),
-            // Box::new(CookieFinder::new("jwt_token")),
-        ])
-        .response_error(false);
+    let auth_handler: JwtAuth<JwtClaims, _> =
+        JwtAuth::new(ConstDecoder::from_secret(SECRET_KEY.as_bytes()))
+            .finders(vec![
+                // Box::new(HeaderFinder::new()),
+                Box::new(QueryFinder::new("jwt_token")),
+                // Box::new(CookieFinder::new("jwt_token")),
+            ])
+            .force_passed(true);
 
     let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
     Server::new(acceptor)
