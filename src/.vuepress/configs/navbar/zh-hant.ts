458f87028e53d571435e7c2110d96cb2246650c8
Chrislearn Young <chris@acroidea.com>
2023-07-06 16:28:36 +0800

    Version 0.45.x

--- src/.vuepress/configs/navbar/zh-hant.ts
+++ src/.vuepress/configs/navbar/zh-hant.ts
@@ -61,14 +61,14 @@ export const navbarZhHant: NavbarConfig = [
     link: '/zh-hant/donate.md',
   },
   {
-    text: `v0.44.x release`,
+    text: `v0.45.x release`,
     children: [
       {
         text: 'main branch',
         link: 'https://next.salvo.rs',
       },
       {
-        text: 'v0.44.x release',
+        text: 'v0.45.x release',
         link: 'https://salvo.rs',
       },
     ],
