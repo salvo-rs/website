187ccac56411d51327f2a778867983ba04a63134
Christopher Young <chris@acroidea.com>
2023-09-05 10:12:22 +0800

    0.54.x

--- src/.vuepress/configs/navbar/versions.ts
+++ src/.vuepress/configs/navbar/versions.ts
@@ -2,14 +2,14 @@
 // import meta from '../meta.js';
 
 export default {
-    text: `v0.53.x release`,
+    text: `v0.54.x release`,
     children: [
         {
             text: 'main branch',
             link: 'https://next.salvo.rs',
         },
         {
-            text: 'v0.53.x release',
+            text: 'v0.54.x release',
             link: 'https://salvo.rs',
         },
     ],
