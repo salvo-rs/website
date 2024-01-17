b66931624916c994b280d29ba1b154a5a344a440
Chrislearn Young <chris@acroidea.com>
2024-01-17 14:11:49 +0800

    version 0.65.x

--- src/.vuepress/configs/navbar/versions.ts
+++ src/.vuepress/configs/navbar/versions.ts
@@ -9,7 +9,7 @@ export default {
             link: 'https://next.salvo.rs',
         },
         {
-            text: 'v0.64.x release',
+            text: 'v0.65.x release',
             link: 'https://salvo.rs',
         },
     ],
